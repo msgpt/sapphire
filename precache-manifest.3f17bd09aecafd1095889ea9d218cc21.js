d07df55b1e6198c2ef83be3b3f49ac864524d637
Miguel <msgpt@msn.com>
2019-10-25 11:44:07 +0100

    Updates

---
+++ precache-manifest.3f17bd09aecafd1095889ea9d218cc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a771001a706c80dddf9f1a312c54e19c",
+    "url": "/sapphire/index.html"
+  },
+  {
+    "revision": "2d6b5e761f32a6861a50",
+    "url": "/sapphire/static/css/main.6fb54acb.chunk.css"
+  },
+  {
+    "revision": "2d94d667af81c7b2c2d8",
+    "url": "/sapphire/static/js/2.6d4d1698.chunk.js"
+  },
+  {
+    "revision": "2d6b5e761f32a6861a50",
+    "url": "/sapphire/static/js/main.bae84550.chunk.js"
+  },
+  {
+    "revision": "844e916919d0717155ae",
+    "url": "/sapphire/static/js/runtime-main.1f6275a5.js"
+  }
+]);
