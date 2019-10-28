fff65f1bef42706bc4a040f2f5a71b73bea1ff73
Miguel <msgpt@msn.com>
2019-10-28 17:49:30 +0000

    Updates

--- precache-manifest.4fe2247d29a24476c0ec24483c608b71.js
+++ precache-manifest.4fe2247d29a24476c0ec24483c608b71.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ffa7ddc7826205508486d1fa5969eb2b",
+    "revision": "c9dadcc69f4d18aae5b48b2498b702cb",
     "url": "/sapphire/index.html"
   },
   {
-    "revision": "7796800ac3c056dd6f15",
-    "url": "/sapphire/static/css/main.6fb54acb.chunk.css"
+    "revision": "bf2be1175a3cbfed67e3",
+    "url": "/sapphire/static/css/main.66292bab.chunk.css"
   },
   {
     "revision": "2d94d667af81c7b2c2d8",
     "url": "/sapphire/static/js/2.6d4d1698.chunk.js"
   },
   {
-    "revision": "7796800ac3c056dd6f15",
+    "revision": "bf2be1175a3cbfed67e3",
     "url": "/sapphire/static/js/main.f9db6567.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sapphire/precache-manifest.53dd21223f636ed92eb1565dd56175e5.js"
+  "/sapphire/precache-manifest.4fe2247d29a24476c0ec24483c608b71.js"
 );
 
 self.addEventListener('message', (event) => {
